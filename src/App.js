62fd241fa21a9d206607dd45072f7c1878cd1b9d
Anish Varshney <anishvarshney912@gmail.com>
2024-04-29 23:32:44 +0530

    In the process of developing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  Routes,
+  Navigate 
+} from "react-router-dom";
+import Success from './pages/Success';
+import { useSelector } from 'react-redux';
 
-function App() {
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path = "/" element={<Home />} />
+      <Route path = "/products/:categories" element = {<ProductList/>} />
+      <Route path="/product/:id" element={<Product/>} />
+      <Route path="/cart" element={<Cart/>} />
+      <Route path="/success" element={<Success/>} />
+      {/* <Route path="/login" element={<Login/>} /> */}
+      <Route path="/login" element={
+        user ? <Navigate to = "/" replace /> : <Login/>} />
+
+      <Route path="/register" element={
+        user ? <Navigate to = "/" replace /> : <Register/>} />
+
+        {/* <Route exact path="/products/:categories">
+          <ProductList />
+        </Route> */}
+      </Routes>
+    </Router>
+
   );
 }
 
